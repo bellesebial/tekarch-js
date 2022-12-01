9394c76512842125738c79faa9e929f5cffd342c
Eloisamarie29 <eloisamarie.desucatan@cit.edu>
2022-12-01 23:32:18 +0800

    sdse

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import CreateBook from './components/CreateBook';
+import UpdateBook from './components/UpdateBook';
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <CreateBook/>
+      <UpdateBook/>
+    
       </header>
     </div>
   );
+
 }
 
 export default App;
