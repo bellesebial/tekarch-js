77f7e9ad9a93d9abba084c20f350744919282c88
Eloisamarie29 <eloisamarie.desucatan@cit.edu>
2022-12-19 22:25:59 +0800

    jijoij

--- src/components/CreateBook.js
+++ src/components/CreateBook.js
@@ -1,11 +1,11 @@
 import './background.css';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import { Breadcrumbs, Link} from '@mui/material';
+import { Breadcrumbs, Grid, Link } from '@mui/material';
 import NavBar from './NavBar';
-import {useState } from 'react';
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 const Create = {
@@ -65,35 +65,45 @@ export default function CreateBook() {
                         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
                             <div>
 
-                                <h4>Enter Book Title</h4>
-                                <TextField id="outlined-basic" variant="outlined" color='success'
-                                    value={title} onChange={(e) => setTitle(e.target.value)} required /> <br /><br />
+                                
+                                    <Grid item xs={2}container rowSpacing={0} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
+                                        <h4>Enter Book Title</h4>
+                                
+                                        <TextField id="outlined-basic" variant="outlined" color='success' sx={{ marginLeft: '2rem'}} 
+                                            value={title} onChange={(e) => setTitle(e.target.value)} required />
+                                    </Grid><br/>
+                                    <Grid item xs={2} container rowSpacing={0} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
+                                        <h4>Enter Author</h4>
 
-                                <h4>Enter Author</h4>
-                                <TextField id="outlined-basic" variant="outlined" color='success'
-                                    value={author} onChange={(e) => setAuthor(e.target.value)} required /><br/>
-
-                                <Box component="span" sx={{ p: 10, border: '1px dashed grey', textAlign: 'right', marginLeft: '20rem', marginTop: '10rem' }} 
-                                value={image} onChange={(e) => setImage(e.target.value)} required>
-                                    <Button >Upload/Drag the photo here</Button>
-                                </Box>
-
-                                <h4>Enter Date Published</h4>
-                                <TextField id="outlined-basic" variant="outlined" color='success'
-                                    value={published} onChange={(e) => setPublished(e.target.value)} required /> <br /><br />
-
-                                <h4>Enter Book Genre</h4>
-                                <TextField id="outlined-basic" variant="outlined" color='success'
-                                    value={genre} onChange={(e) => setGenre(e.target.value)} required /> <br /><br />
-
-                                <h4>Enter Synopsis</h4>
-                                <TextField id="outlined-basic" variant="outlined" color='success'
-                                    value={synopsis} onChange={(e) => setSynopsis(e.target.value)} required /> <br /><br />
-
+                                        <TextField id="outlined-basic" variant="outlined" color='success' sx={{ marginLeft: '3rem'}} 
+                                            value={author} onChange={(e) => setAuthor(e.target.value)} required />
+                                    </Grid><br/>
+                                    <Box component="span" sx={{ p: 10, border: '1px dashed grey', textAlign: 'right', marginLeft: '30rem', marginTop: '10rem' }}
+                                        value={image} onChange={(e) => setImage(e.target.value)} required>
+                                        <Button >Upload/Drag the photo here</Button>
+                                    </Box>
+                                    <Grid item xs={2} container rowSpacing={0} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
+                                        <h4>Enter Date Published</h4>
+                                
+                                        <TextField id="outlined-basic" variant="outlined" color='success' sx={{ marginLeft: '1rem'}}
+                                            value={published} onChange={(e) => setPublished(e.target.value)} required />
+                                    </Grid><br/>
+                                    <Grid item xs={2} container rowSpacing={0} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
+                                        <h4>Enter Book Genre</h4>
+                          
+                                        <TextField id="outlined-basic" variant="outlined" color='success' sx={{ marginLeft: '2rem'}}
+                                            value={genre} onChange={(e) => setGenre(e.target.value)} required />
+                                    </Grid><br/>
+                                    <Grid item xs={2} container rowSpacing={0} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
+                                        <h4>Enter Synopsis</h4>
+                                
+                                        <TextField id="outlined-basic" variant="outlined" color='success' sx={{ marginLeft: '2rem'}}
+                                            value={synopsis} onChange={(e) => setSynopsis(e.target.value)} required />
+                                    </Grid>
 
                                 <Link to="/tbrlist" style={{ textDecoration: 'inherit' }}>
-                                    <Button variant="contained" sx={{ width: 150, height: 50, marginLeft: 4, marginRight: 4, marginTop: 3, marginBottom: 3 }} 
-                                    onClick={(e) => handleSubmit(e)}>Add Book</Button>
+                                    <Button variant="contained" sx={{ width: 150, height: 50, marginLeft: 4, marginRight: 4, marginTop: 3, marginBottom: 3 }}
+                                        onClick={(e) => handleSubmit(e)}>Add Book</Button>
                                 </Link>
                                 <Button sx={{ width: 150, height: 50, marginLeft: 4, marginRight: 4 }} variant="contained" href="/tbrlist">Cancel</Button>
 
